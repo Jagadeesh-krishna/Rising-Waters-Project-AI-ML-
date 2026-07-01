4ee0e623f145b809669ebc3d98a6b2aa0c829773
Jagadeesh-Krishna <govindakrishna06@gmail.com>
2026-07-01 12:05:49 +0530

    Fix predict route

--- app.py
+++ app.py
@@ -30,16 +30,21 @@ def predict():
         data = np.array([[annual, janfeb, marmay, junsep, octdec]])
         data = scaler.transform(data)
 
+        # Prediction
         prediction = model.predict(data)[0]
 
+        # Prediction probability
+        probability = model.predict_proba(data)[0]
+
+        # Probability of flood (class 1)
+        risk = round(probability[1] * 100)
+
         if prediction == 1:
-            return render_template("flood.html")
+            return render_template("flood.html", risk=risk)
         else:
-            return render_template("noflood.html")
+            return render_template("noflood.html", risk=risk)
 
     except Exception as e:
         return f"Error: {e}"
-
-
 if __name__ == "__main__":
     app.run(debug=True)
